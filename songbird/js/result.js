3af9fb802ba52cd0b693a46a4877ada42e4280c4
Arina <akira657@mail.ru>
2022-11-20 16:36:33 +0300

    feat: implement get  quiz result from localStorage and render heading

---
+++ songbird/js/result.js
@@ -0,0 +1,9 @@
+import { createDomNode } from "./createDomNode.js";
+
+const score = localStorage.getItem('score');
+
+const resultTitle = document.querySelector('.result__title');
+const resultSubtitle = createDomNode('h3', 'result__subtitle');
+resultSubtitle.textContent = `Вы прошли викторину и набрали ${score} из 30 возможных баллов`;
+
+resultTitle.after(resultSubtitle);
